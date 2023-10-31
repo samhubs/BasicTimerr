2c00bdcfb2540e722c24fe78380ba126f630ea6a
Sameer <sameersharma.dc@gmail.com>
2023-10-30 20:21:46 -0600

    Added comments alonwith the Readme file.

--- BasicTimerr/ViewController.swift
+++ BasicTimerr/ViewController.swift
@@ -1,19 +1,63 @@
-//
-//  ViewController.swift
-//  BasicTimerr
-//
-//  Created by Sameer Sharma on 2023-10-30.
-//
-
+// Importing the UIKit library, which provides the necessary infrastructure for your iOS or tvOS apps.
 import UIKit
 
+// ViewController is a subclass of UIViewController, which is a fundamental building block of iOS apps.
 class ViewController: UIViewController {
-
+    
+    // Declaring a Timer object that will be used to update the clock display.
+    var timer = Timer()
+    // Declaring a boolean variable to track whether the timer is paused.
+    var isPause = false
+    // Declaring an integer variable to track the remaining time, initialized to 210 seconds.
+    var time = 210
+    
+    // Connecting the clockDisplay label from the storyboard to the code.
+    @IBOutlet weak var clockDisplay: UILabel!
+    
+    // Defining a function that decreases the timer by 1 every second and updates the clock display.
+    @objc func decreaseTimer() {
+        if time > 0 {
+            time -= 1
+            clockDisplay.text = String(time)
+        } else {
+            timer.invalidate() // If time runs out, stop the timer.
+        }
+    }
+    
+    // Defining an action for the play button. When tapped, this function starts the timer.
+    @IBAction func playButton(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.decreaseTimer), userInfo: nil, repeats: true)
+    }
+    
+    // Defining an action for the pause button. When tapped, this function pauses the timer.
+    @IBAction func pauseButton(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    // Defining an action to decrease the timer by 10 seconds.
+    @IBAction func timerResetNegative(_ sender: Any) {
+        time -= 10
+        clockDisplay.text = String(time)
+    }
+    
+    // Defining an action to increase the timer by 10 seconds, with a maximum limit of 210 seconds.
+    @IBAction func timerResetPositive(_ sender: Any) {
+        time += 10
+        if time < 210 {
+            clockDisplay.text = String(time)
+        } else {
+            timer.invalidate() // If time exceeds 210 seconds, set it back to 210 and stop the timer.
+        }
+    }
+    
+    // Defining an action to reset the timer back to 210 seconds.
+    @IBAction func resetButton(_ sender: Any) {
+        time = 210
+    }
+    
+    // A system callback function called when the view is loaded into memory.
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
---
+++ ReadME.md
@@ -0,0 +1,56 @@
+
+---
+
+# Basic Timer App
+
+A simple and user-friendly timer app built with Swift for iOS. Keep track of your activities with ease and get notified when the time is up!
+
+## Features
+
+- **Countdown Timer**: Set a timer for up to 210 seconds and watch as it counts down to zero.
+- **Play/Pause**: Start the timer and pause it whenever you need.
+- **Reset**: Quickly reset the timer back to the initial 210 seconds.
+- **Adjust Time**: Add or subtract time from the timer in 10-second intervals.
+
+## How to Use
+
+1. **Start Timer**: Tap the "Play" button to start the countdown.
+2. **Pause Timer**: Tap the "Pause" button to temporarily stop the timer.
+3. **Reset Timer**: Tap the "Reset" button to reset the timer back to 210 seconds.
+4. **Adjust Time**: 
+   - Tap the "+" button to add 10 seconds to the timer.
+   - Tap the "-" button to subtract 10 seconds from the timer.
+
+## Requirements
+
+- iOS 13.0+
+- Xcode 11.0+
+
+## Installation
+
+1. Clone the repository to your local machine.
+   ```sh
+   git clone https://github.com/your_username/BasicTimer.git
+   ```
+2. Open the project in Xcode.
+3. Build and run the app on your preferred iOS simulator or device.
+
+## Contributing
+
+We welcome contributions to make this app even better! Here are ways you can contribute:
+
+- [Report bugs](https://github.com/your_username/BasicTimer/issues)
+- Suggest new features or improvements
+- Improve the existing code or fix bugs
+- Help improve the documentation
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
+
+## Acknowledgements
+
+Thank you to everyone who has contributed to the development of this app!
+
+---
+
